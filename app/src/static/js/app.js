6c34f71c0c13f4b58c4d227aa75c95fa94c98a73
LukeBasco1216 <basco.luke@iisgalvanimi.edu.it>
2023-12-06 12:45:43 +0000

    using bind mounts

--- app/src/static/js/app.js
+++ app/src/static/js/app.js
@@ -106,7 +106,7 @@ function AddItemForm({ onNewItem }) {
                         disabled={!newItem.length}
                         className={submitting ? 'disabled' : ''}
                     >
-                        {submitting ? 'Adding...' : 'Add Item'}
+                        {submitting ? 'Adding...' : 'Add new item that you deem kewl'}
                     </Button>
                 </InputGroup.Append>
             </InputGroup>
